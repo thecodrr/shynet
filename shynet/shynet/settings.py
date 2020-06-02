e66e2116850abae7560798b0b6b826a9da7b75a3
Abdullah Atta <thecodrr@protonmail.com>
2020-06-02 08:53:22 +0500

    fix: 403 error due to CSRF verification failure

--- shynet/shynet/settings.py
+++ shynet/shynet/settings.py
@@ -31,6 +31,7 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
 
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
 
 # Application definition
 
